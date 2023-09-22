96f507d480552c87d14c9dc8b755438113da0c18
Barissa-Imran <imranbarissa@gmail.com>
2023-09-22 23:38:41 +0300

    add 4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,7 +3,7 @@
 const sinon = require('sinon');
 const Utils = require('./utils');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses  the calculateNumber method of Utils', () => {
